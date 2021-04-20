0e28f4ec8fb5e3f8a5ad3cbb0524df5d6166f085
letha11 <ibkaanhar1@gmail.com>
2021-04-20 05:05:38 +0000

    fix endpoint

--- helper/helper.js
+++ helper/helper.js
@@ -5,10 +5,10 @@ class Helper {
   scrapeListManga(response) {
     const $ = cheerio.load(response.data);
     const element = $("#content > div.wrapper > div.postbody > div.bixbox");
-    const searchElement = $("#content > div.wrapper > div.postbody > div.bixbox");
+    // const searchElement = $("#content > div.wrapper > div.postbody > div.bixbox");
     let listManga = [];
-    let searchResult = [];
-    let searchPagination;
+    // let searchResult = [];
+    // let searchPagination;
     let searchResultObj = {};
     let listMangaObj = {};
 
@@ -23,38 +23,11 @@ class Helper {
       rating = $(el)
         .find("div.bsx > a > div.bigor > div.adds div.numscore")
         .text();
-      endpoint = title
-        .replace(/[^\w\s-]|(.)(?=\1)/g, "")
-        .replace(/\s/g, "-")
-        .replace(/[^\w\s-]|(.)(?=\1)/g, "")
-        .toLowerCase();
-
-      searchResult.push({
-        title,
-        thumb,
-        latest_chapter,
-        rating,
-        endpoint,
-      });
-    });
-    searchResultObj.manga_list = searchResult;
-
-    // Getting for mangaList
-    element.find("div.mrgn > div.listupd > div.bs").each((i, el) => {
-      let title, thumb, latest_chapter, rating, endpoint;
-      title = $(el).find("div.bsx > a > div.bigor > div.tt").text().trim();
-      thumb = $(el).find("div.bsx > a > div.limit > img").attr("src");
-      latest_chapter = $(el)
-        .find("div.bsx > a > div.bigor > div.adds > div.epxs")
-        .text();
-      rating = $(el)
-        .find("div.bsx > a > div.bigor > div.adds div.numscore")
-        .text();
-      endpoint = title
-        .replace(/[^\w\s-]|(.)(?=\1)/g, "")
-        .replace(/\s/g, "-")
-        .replace(/[^\w\s-]|(.)(?=\1)/g, "")
-        .toLowerCase();
+      endpoint = $(el)
+        .find("div.bsx > a")
+        .attr('href')
+        .replace('https://kiryuu.co/manga/', '')
+        .replace(/\//g, '')
 
       listManga.push({
         title,
@@ -64,6 +37,33 @@ class Helper {
         endpoint,
       });
     });
+    searchResultObj.manga_list = listManga;
+
+    // Getting for mangaList
+    // element.find("div.mrgn > div.listupd > div.bs").each((i, el) => {
+    //   let title, thumb, latest_chapter, rating, endpoint;
+    //   title = $(el).find("div.bsx > a > div.bigor > div.tt").text().trim();
+    //   thumb = $(el).find("div.bsx > a > div.limit > img").attr("src");
+    //   latest_chapter = $(el)
+    //     .find("div.bsx > a > div.bigor > div.adds > div.epxs")
+    //     .text();
+    //   rating = $(el)
+    //     .find("div.bsx > a > div.bigor > div.adds div.numscore")
+    //     .text();
+    //   endpoint = title
+    //     .replace(/[^\w\s-]|(.)(?=\1)/g, "")
+    //     .replace(/\s/g, "-")
+    //     .replace(/[^\w\s-]|(.)(?=\1)/g, "")
+    //     .toLowerCase();
+
+    //   listManga.push({
+    //     title,
+    //     thumb,
+    //     latest_chapter,
+    //     rating,
+    //     endpoint,
+    //   });
+    // });
     listMangaObj.manga_list = listManga;
 
     return [searchResultObj, listMangaObj];
